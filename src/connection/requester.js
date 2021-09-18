1e34c84e5c1b46c994d6b17761386f599981518a
nate-crema <58645920+nate-crema@users.noreply.github.com>
2021-09-18 23:29:52 +0900

    set connectoin with ssl certificate

--- src/connection/requester.js
+++ src/connection/requester.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `http://${process.env.REACT_APP_BACKEND_HOST || "localhost:3001"}`,
+    baseURL: `https://${process.env.REACT_APP_BACKEND_HOST || "localhost:3001"}`,
     timeout: 5000
 });
 
