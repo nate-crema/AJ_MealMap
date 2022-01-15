15b9e18cb5480ade79755ea2429ecca4d1e0ef97
waffle <nate.crema@gmail.com>
2022-01-16 02:22:31 +0900

    Add Swipe-Up Function & Animation
    * Add Swipe Up Function with Animation (/mobile/components/submenu/Manage/ManageMeeting.js)

--- src/mobile/components/submenu/Manage/ManageMeeting.js
+++ src/mobile/components/submenu/Manage/ManageMeeting.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 // img
@@ -26,14 +26,142 @@ function MeetingInvitation({ info }) {
         second: info.meet_time.getSeconds(),
     }, filterImgList = {
         "[_id]": ""
+    };
+
+    // swipe detection
+
+    const [ eventList, setEL ] = useState({
+        toUp: {},
+        toDown: {},
+        toLeft: {},
+        toRight: {}
+    });
+    const [ swipe_start, setSS ] = useState([-100, -100]);
+
+    const SWIPE_STD = 50;
+
+    const _swipeStartHandler = (e) => {
+        // let is_ignore = false;
+        // e.nativeEvent.path.forEach(v => {
+        //     for (var i = 0; i < v?.classList?.length || 0; i++) (v.classList[i] === "slider") && (is_ignore = true);
+        // })
+        // if (is_ignore) return setSS([-100, -100]);
+        setSS([e.touches[0].clientX, e.touches[0].clientY]);
     }
 
-    useEffect(() => console.log(info), []);
+    const _swipeOngoingHandler = (e) => {
+        // console.log("end");
+        // console.log();
+        if (swipe_start[0] < 0 || swipe_start[1] < 0) {
+            // console.log("ignored");
+            return;
+        }
+        let mp = null;
+        if (swipe_start[0] + SWIPE_STD - e.touches[0].clientX < 0) mp = "toRight";
+        else if (swipe_start[0] - SWIPE_STD - e.touches[0].clientX > 0) mp = "toLeft";
+        else if (swipe_start[1] + SWIPE_STD - e.touches[0].clientY < 0) mp = "toDown";
+        else if (swipe_start[1] - SWIPE_STD - e.touches[0].clientY > 0) mp = "toUp";
+        
+        // console.log(mp);
+        // console.log(eventList[mp]);
+        const t = new Date().getTime();
+        
+        if ( mp ) {
+            console.log("Swipe Detection");
+            Object.values(eventList[mp]).forEach((fnc, i) => {
+                try {
+                    fnc(
+                        (mp === "toRight") ? (e.touches[0].clientX - swipe_start[0]) :
+                        (mp === "toLeft") ? (swipe_start[0] - e.touches[0].clientX) :
+                        (mp === "toDown") ? (e.touches[0].clientY - swipe_start[1]) :
+                        (mp === "toUp") ? (swipe_start[1] - e.touches[0].clientY) :
+                        0
+                    )
+                } catch(e) {
+                    console.log(`Event Listener: Error on ${i} | ${fnc.name}`);
+                    console.error(e);
+                }
+            })
+        }
+        // else console.log("ignored");
+    }
 
-    return <div className="meeting-invitation">
+    const _swipeEvent = new function() {
+        this.addEventListener = function(event, fnc) {
+            const _new = eventList;
+            _new[event][fnc.name || Object.values(eventList[event]).length] = fnc;
+            setEL(_new);
+            // console.log(_new);
+        }
+        this.removeEventListener = function(event, fnc) {
+            const _new = eventList;
+            delete _new[event][fnc.name];
+            setEL(_new);
+            // console.log(_new);
+        }
+        return this;
+    }
+
+
+    // swipe event registration
+
+    useEffect(() => {
+        _swipeEvent.addEventListener("toUp", _cardSharingDesignHandler);
+        return () => {
+            _swipeEvent.removeEventListener("toUp", _cardSharingDesignHandler);
+        }
+    }, [])
+
+    // card sharing handler
+
+    const [ bg_automize, setBGAutomize ] = useState(false);
+    const [ sharing_bg, setSharingBg ] = useState(0);
+    const [ maximum_bg, setMaxBg ] = useState(0);
+    const cardRef = useRef();
+
+    const _cardSharingDesignHandler = (e) => {
+        setSharingBg(e);
+    }
+
+    const _cardSharingHandler = useCallback(() => {
+        const swipe_percent = sharing_bg / maximum_bg;
+        if (swipe_percent > 0.7) {
+            // swiped
+            setBGAutomize(true);
+            setSharingBg(maximum_bg);
+            setTimeout(() => {
+                setSharingBg(0);
+                setTimeout(() => {
+                    setBGAutomize(false);
+                }, 350);
+            }, 1500);
+        } else {
+            // non-swiped
+            setBGAutomize(true);
+            setSharingBg(0);
+            setTimeout(() => {
+                setBGAutomize(false);
+            }, 350);
+        }
+    }, [ sharing_bg ])
+
+    useEffect(() => {
+        setMaxBg(cardRef.current.getBoundingClientRect().height);
+    }, [])
+
+    return <div className="meeting-invitation"
+        onTouchStart={_swipeStartHandler}
+        onTouchMove={_swipeOngoingHandler}
+        onTouchEnd={_cardSharingHandler}
+        ref={cardRef}
+        style={{
+            transition: bg_automize ? "all .5s cubic-bezier(0.5, 1.9, 0.38, 0.7)" : null,
+            marginTop: `-${sharing_bg / 10}px`
+        }}
+    >
         <div className="share-text">
             <img src={ shareIcon }/>
-            <span>카드를 밀어 공유</span>
+            <span>{sharing_bg === 0 ? "카드를 밀어 공유" : "공유대상 확인중"}</span>
         </div>
         <div className="meeting-content">
             <p className="meeting-title">{ meeting_created.month }월 { meeting_created.date }일에 초대된 약속</p>
@@ -52,6 +180,11 @@ function MeetingInvitation({ info }) {
             </div>
         </div>
         <img className="background-style-img" src={ calenderBackground }/>
+        <div className="sharing-swipe-background" style={{
+            transition: bg_automize ? "all .35s ease" : "none",
+            height: `${sharing_bg}px`,
+            opacity: sharing_bg / maximum_bg
+        }}/>
     </div>
 }
 
