925afed6620acf28bd035b4cf1cc722ba30c5d7b
waffle <nate.crema@gmail.com>
2022-06-23 16:19:56 +0900

    InfoMenu component 분기
    /components/organism/Subdisplay/InfoDisplay/index.tsx
    * InfoMenu component 분기 (사유: 컴포넌트 세분화 및 Atomic design 준수)
    
    /components/organism/Subdisplay/InfoDisplay/ActionBtn.tsx
    /components/organism/Subdisplay/InfoDisplay/BasicInfo.tsx
    /components/organism/Subdisplay/InfoDisplay/ReviewBrief.tsx
    * InfoMenu 디자인 재구성을 위한 삭제 (-> ./deprecated로 이동 / 다음 commit에서 제거)

--- src/components/organism/Subdisplay/InfoDisplay/index.tsx
+++ src/components/organism/Subdisplay/InfoDisplay/index.tsx
@@ -15,106 +15,16 @@ import MarkerImg from '@assets/Marker.png';
 
 // components
 import ServiceTitler from "../../../molecule/Titler";
+import InfoMenu from "./InfoMenu";
 import InfoHeader from "./InfoHeader";
-import BasicInfo from "./InfoMenu/BasicInfo";
-import ReviewBrief from "./InfoMenu/ReviewBrief";
-import ActionBtn from "./InfoMenu/ActionBtn";
 
 // interfaces
 // import { shopReview } from "@interfaces/shop/Review";
 import { ShopAPIResult } from "@interfaces/api/service";
 import { ShopIDType, ShopServiceType } from "@interfaces/service/service.data.types/Shop";
 
-type touchState = "na" | "toUp" | "toDown";
-type changeState = "standby" | "fadeIn" | "fadeOut";
-
-type InfoMenuProps = {
-    contact?: string
-    duration?: string
-    // reviews?: Array<shopReview>
-    reviews?: Array<any>
-    setMinimize: Function
-}
-
 type InfoBlockProps = {
     shop: ShopServiceType
-}
-
-const InfoMenu: React.FC<InfoMenuProps> = ({ contact, duration, reviews, setMinimize }) => {
-
-    const [ menu, setMenu ] = useState<number>(0);
-    const [ menu_changing, setMenuChanging ] = useState<[ touchState, changeState ]>( [ "na", "standby" ] );
-    const MENU_ANIMATE_TAGS = useMemo( () => ( menu_changing.map( v => ` animation_${v}` ).toString().replace(",", "") ), [ menu_changing ] )
-
-    const MENU_EQUAL = useCallback( ( id: number ): boolean => menu === id, [ menu ] );
-
-    const setMenuControl = useCallback( ( touchState: touchState ): void => {
-        if ( touchState === "toUp" && ( menu < 2 ) ) {
-            setMinimize( false );
-            setMenuChanging( [ touchState, "fadeOut" ] );
-            setTimeout(() => {
-                setMenu( p => p+1 );
-                setMenuChanging( [ "toDown", "fadeOut" ] );
-                setTimeout(() => setMenuChanging( [ "na", "fadeIn" ] ), 100);
-            }, 300);
-        }
-        else if ( touchState === "toDown" && ( menu > -1 ) ) {
-            setMenuChanging( [ touchState, "fadeOut" ] );
-            // console.log( menu );
-            if ( menu === 0 ) setMinimize( true );
-            setTimeout(() => {
-                setMenu( p => p-1 );
-                setMenuChanging( [ "toUp", "fadeOut" ] );
-                setTimeout(() => setMenuChanging( [ "na", "standby" ] ), 100);
-            }, 300);
-        }
-    }, [ menu ] );
-
-
-
-    const SWIPE_STANDARD = 50;
-
-    const initTouchPosition = useRef<[ number, number ]>( [ -1, -1 ] );
-    const [ touchState, setTouchState ] = useState<touchState>( "na" );
-    const STATEBAR_TOP = useMemo( () => `calc( 33% * ${ menu })`, [ menu ] )
-
-    const setTouchStateControl = ( e: SyntheticEvent ): void => {
-        const { changedTouches }: TouchList | any = e.nativeEvent;
-
-        if ( !changedTouches.length ) return;
-        const { clientX: x, clientY: y }: Touch = changedTouches[0];
-
-        const STATES = {
-            TOUCH_STARTED: initTouchPosition.current.includes(-1),
-            SWIPE_TODOWN: y > initTouchPosition.current[1] + SWIPE_STANDARD,
-            SWIPE_TOUP: y + SWIPE_STANDARD < initTouchPosition.current[1]
-        }
-
-        if ( STATES.TOUCH_STARTED ) {
-            setTouchState( "na" );
-            initTouchPosition.current = [ x, y ];
-        }
-        else if ( STATES.SWIPE_TODOWN ) setTouchState( "toDown" );
-        else if ( STATES.SWIPE_TOUP ) setTouchState( "toUp" );
-        else setTouchState( "na" );
-    }
-
-    useEffect(() => setMenuControl( touchState ), [ touchState ]);
-    
-
-    return <div className="info-menu" onTouchMove={ setTouchStateControl }>
-        <div className={ "menu-content-wrap" + MENU_ANIMATE_TAGS }>
-            {
-                MENU_EQUAL(0) ? <BasicInfo contact={ contact || "연락처정보 없음" } duration={ duration || "거리정보 없음" }/> :
-                MENU_EQUAL(1) ? <ReviewBrief reviews={ reviews || [] } /> :
-                MENU_EQUAL(2) ? <ActionBtn/> :
-                <></>
-            }
-        </div>
-        <div className="menu-statebar">
-            <div className="menu-onstate" style={{ top: STATEBAR_TOP }}/>
-        </div>
-    </div>
 }
 
 // const InfoBlock: React.FC<InfoBlockProps> = ({ shop }) => {
